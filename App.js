a73e10e23ef948d38fd05d9e0eecbda0bb5958ed
David Peralez <davidperalez89@gmail.com>
2024-04-29 11:49:06 -0700

    created three tabs

--- App.js
+++ App.js
@@ -2,22 +2,8 @@ import { StyleSheet, Text, View } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import Quote from "./tabs/Quote";
-
-function HomeScreen() {
-  return (
-    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-      <Text>Home!</Text>
-    </View>
-  );
-}
-
-function SettingsScreen() {
-  return (
-    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-      <Text>Settings!</Text>
-    </View>
-  );
-}
+import HomeScreen from "./tabs/HomeScreen.js";
+import SettingsScreen from "./tabs/SettingsScreen.js";
 
 const Tab = createBottomTabNavigator();
 
